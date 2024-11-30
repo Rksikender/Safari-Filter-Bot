f463bdd0f2bc1d2ffdb5480760ec4f737249a004
K274403 <158284550+K274403@users.noreply.github.com>
2024-11-30 18:46:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ OWNER_USER_NAME = environ.get("OWNER_USER_NAME", "looterekrish") # widout 👉 @
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002148604082').split()]
 # post channel auto post new movie
 POST_CHANNELS = list(map(int, (channel.strip() for channel in environ.get('POST_CHANNELS', '-1002148604082').split(','))))
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002177424257'))
+AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002023191399'))
 AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002023191399'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
